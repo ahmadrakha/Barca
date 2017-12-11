5279270a649344996ceccd54bedba24b3100e4db
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 06:17:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function showText(elem){
-	elem.innerHTML = "JavaScriptSorcery";
+            document.getElementById("one").innerHTML = "<center><p>POSITION: STRIKER<br><br>GAMES STARTED<br>33<br><br>GOALS SCORED<br>28<br><br>ASSISTS<br>13<br><br>GOAL/GAME RATIO<br>0.848%<br></center>
+";
 }
