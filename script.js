429276f967f37c3e0a175e2d52730c0a48143212
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 10:07:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function mes(elem){
-            document.getElementById("one").innerHTML = "Messi games started 553                                Goals 536 Assists 186";
+            document.getElementById("one").innerHTML = "MESSI<br><br>Position: Winger<br><br>Games Started: 553<br><br>Goals: 536<br><br>Assists:186";
 }
 function ron(elem){
-            document.getElementById("two").innerHTML = "Ronaldinho<br><br>Position: LW/CAM<br><br>Games Started: 243<br><br>Goals: 104<br><br>Assists:44";
+            document.getElementById("two").innerHTML = "RONALDINHO<br><br>Position: Attacking Midfielder<br><br>Games Started: 243<br><br>Goals: 104<br><br>Assists:44";
 }
 function xav(elem){
-            document.getElementById("three").innerHTML = "Position: CM";
+            document.getElementById("three").innerHTML = "XAVI<br><br>Position: Midfielder<br><br>Games Started: 578<br><br>Goals: 77<br><br>Assists:108";
 }
 function puy(elem){
-            document.getElementById("four").innerHTML = "Position: RW";
+            document.getElementById("four").innerHTML = "PUYOL<br><br>Position: Defender<br><br>Games Started: 593<br><br>Goals: 18<br><br>Assists:44";
 }
 function sua(elem){
-            document.getElementById("five").innerHTML = "Position: RW";
+            document.getElementById("five").innerHTML = "SUAREZ<br><br>Position: Striker<br><br>Games Started: 192<br><br>Goals: 133<br><br>Assists:63";
 }
 function ney(elem){
-            document.getElementById("six").innerHTML = "Position: RW";
+            document.getElementById("six").innerHTML = "NEYMAR<br><br>Position: Winger<br><br>Games Started: 243<br><br>Goals: 104<br><br>Assists:63";
 }
