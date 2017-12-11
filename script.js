a8a03b1fbe979c9ba4fdbe7bfa3d3092a728162c
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 09:03:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function showText(elem){
+function m(elem){
             document.getElementById("one").innerHTML = "Messi games started 553 Goals 536 Assists 186";
 }
-function ron(elem){
+function r(elem){
+            document.getElementById("two").innerHTML = "Position: RW";
+}
+function x(elem){
+            document.getElementById("two").innerHTML = "Position: RW";
+}
+function p(elem){
+            document.getElementById("two").innerHTML = "Position: RW";
+}
+function s(elem){
+            document.getElementById("two").innerHTML = "Position: RW";
+}
+function n(elem){
             document.getElementById("two").innerHTML = "Position: RW";
 }
