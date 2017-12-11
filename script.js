f80905ab0414647c8a01a01a64acea6886927501
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 11:52:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function resetAll(elem){
+	elem.classList.toggle("gray");
+	document.getElementById("one").classList.toggle("blue1");
+	document.getElementById("two").classList.toggle("red1");
+	document.getElementById("three").classList.toggle("blue2");
+        document.getElementById("four").classList.toggle("red2");
+	document.getElementById("five").classList.toggle("blue3");
+	document.getElementById("six").classList.toggle("red3");
+}
+function resetAll(elem){
+	document.getElementById("one").innerHTML = "";
+	document.getElementById("two").innerHTML = "";
+	document.getElementById("three").innerHTML = "";
+	document.getElementById("four").innerHTML = "";
+        document.getElementById("five").innerHTML = "";
+        document.getElementById("six").innerHTML = "";
+}
+
 function mes(elem){
             document.getElementById("one").innerHTML = "MESSI<br><br>Position: Winger<br><br>Games Started: 553<br><br>Goals: 536<br><br>Assists:186";
 }
@@ -17,20 +35,4 @@ function ney(elem){
             document.getElementById("six").innerHTML = "NEYMAR<br><br>Position: Winger<br><br>Games Started: 243<br><br>Goals: 104<br><br>Assists:63";
 }
 
-function resetAll(elem){
-	document.getElementById("one").innerHTML = "";
-	document.getElementById("two").innerHTML = "";
-	document.getElementById("three").innerHTML = "";
-	document.getElementById("four").innerHTML = "";
-        document.getElementById("five").innerHTML = "";
-        document.getElementById("six").innerHTML = "";
-}
-function resetAll(elem){
-	elem.classList.toggle("gray");
-	document.getElementById("one").classList.toggle("blue1");
-	document.getElementById("two").classList.toggle("red1");
-	document.getElementById("three").classList.toggle("blue2");
-        document.getElementById("four").classList.toggle("red2");
-	document.getElementById("five").classList.toggle("blue3");
-	document.getElementById("six").classList.toggle("red3");
-}
+
