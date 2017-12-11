4b1b2aa9dad54caa8971af9fa135e7d7f14ce17f
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 06:57:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function showText(elem){
             document.getElementById("one").innerHTML = "Position: RW";
             document.getElementById("one").innerHTML = "Position: 395";
 }
+function showTex(elem){
+            document.getElementById("two").innerHTML = "Position: RW";
+}
