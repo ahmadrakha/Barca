1c92fccbc1526a41f30b1f82cbb3df2a13b7fc23
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 10:32:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,8 @@ function sua(elem){
 function ney(elem){
             document.getElementById("six").innerHTML = "NEYMAR<br><br>Position: Winger<br><br>Games Started: 243<br><br>Goals: 104<br><br>Assists:63";
 }
+
+function mes(elem){
+	elem.classList.toggle("messi.png");
+	document.getElementById("one").classList.toggle("blue1");
+}
