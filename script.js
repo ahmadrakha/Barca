fa7bb6bd16ab93813cc05273cf8431f998f0dd8a
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 05:50:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function showText(elem){
+function m(elem){
 	document.getElementById("one").classList.toggle("messi2");
+}
+function r(elem){
+	document.getElementById("two").classList.toggle("messi2");
+}
+function x(elem){
+	document.getElementById("three").classList.toggle("messi2");
+}
+function p(elem){
+	document.getElementById("four").classList.toggle("messi2");
+}
+function s(elem){
+	document.getElementById("five").classList.toggle("messi2");
+}
+function n(elem){
+	document.getElementById("six").classList.toggle("messi2");
 }
