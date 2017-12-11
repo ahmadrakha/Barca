3f95875d9786529d43bffcf2ec37b306e3631d1a
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 10:52:17 -0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function xav(elem){
             document.getElementById("three").innerHTML = "XAVI<br><br>Position: Midfielder<br><br>Games Started: 578<br><br>Goals: 77<br><br>Assists:108";
 }
 function puy(elem){
-            document.getElementById("four").innerHTML = "PUYOL<br><br>Position: Defender<br><br>Games Started: 593<br><br>Goals: 18<br><br>Assists:44";
+            document.getElementById("four").innerHTML = "PUYOL<br><br>Position: Defender<br><br>Games Started: 593<br><br>Goals: 18<br><br>Assists:5";
 }
 function sua(elem){
             document.getElementById("five").innerHTML = "SUAREZ<br><br>Position: Striker<br><br>Games Started: 192<br><br>Goals: 133<br><br>Assists:63";
