9d00f3f1492b50c20e7c31aea7fa54b1ebdaa7f5
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 05:51:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ function m(elem){
 	document.getElementById("one").classList.toggle("messi2");
 }
 function r(elem){
-	document.getElementById("two").classList.toggle("messi2");
+	document.getElementById("two").classList.toggle("ronaldhino2");
 }
 function x(elem){
-	document.getElementById("three").classList.toggle("messi2");
+	document.getElementById("three").classList.toggle("xavi2");
 }
 function p(elem){
-	document.getElementById("four").classList.toggle("messi2");
+	document.getElementById("four").classList.toggle("puyol2");
 }
 function s(elem){
-	document.getElementById("five").classList.toggle("messi2");
+	document.getElementById("five").classList.toggle("suarez2");
 }
 function n(elem){
-	document.getElementById("six").classList.toggle("messi2");
+	document.getElementById("six").classList.toggle("neymar2");
 }
