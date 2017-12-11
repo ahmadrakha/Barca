b03e50d92c720aae8c687c2a52662e388613d7f7
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 06:19:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 function showText(elem){
-            document.getElementById("one").innerHTML = "<center><p>POSITION: STRIKER<br><br>GAMES STARTED<br>33<br><br>GOALS SCORED<br>28<br><br>ASSISTS<br>13<br><br>GOAL/GAME RATIO<br>0.848%<br></center>
-";
+            document.getElementById("one").innerHTML = "Position: RW         Goals: 592";
 }
