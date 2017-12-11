04bb88cd331baa1532d468666ef511a57920fe48
ahmadrakha <ahmad_rakha@davincischools.org>
2017-12-11 06:05:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,3 @@ function showText(elem){
 	elem.innerHTML = "JavaScriptSorcery";
 	document.getElementById("one").classList.toggle("messi2");
 }
-function sText(elem){
-	document.getElementById("two").classList.toggle("ronaldhino2");
-}
